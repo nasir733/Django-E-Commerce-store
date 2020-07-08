43fc62f1a7267b215afbf800e3e9c2f47f1a03ba
nasir733 <engnasir733@gmail.com>
2020-07-08 10:06:42 +0500

    add

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("contact/", views.contact, name="ContactUs"),
     path("tracker/", views.tracker, name="TrackingStatus"),
     path("search/", views.search, name="Search"),
-    path("productview/", views.productView, name="ProductView"),
-    path("checkout/", views.checkout, name="Checkout"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
 ]
